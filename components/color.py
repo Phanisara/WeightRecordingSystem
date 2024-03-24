4a3665c2e524d4f75aa5b5e6f6502165a6c46c90
mildphan <85720997+Phanisara@users.noreply.github.com>
2024-03-24 17:15:44 +0700

    Add the color code in class painterColor

--- components/color.py
+++ components/color.py
@@ -13,16 +13,22 @@ class Color():
   browngreen    = "rgb(106, 103, 19)"
   lightbrown    = "rgba(192, 190, 139, 0.4)"
   birch         = "rgb(59, 53, 40)"
+  gray          = "rgb(133, 133, 133)"
+  darkgray      = "rgb(50, 50, 50)"
 
 class PainterColor():
-  white         = QColor(255, 255, 255)
-  lightpink     = QColor(255, 248, 248)
-  middlepink    = QColor(221, 194, 194)
-  lightburgundy = QColor(156, 78, 78)
-  darkred       = QColor(72, 8, 8)
-  lightgreen    = QColor(157, 182, 170, 60)
-  middlegreen   = QColor(103, 126, 112, 80) 
-  darkgreen     = QColor(40, 59, 48)
-  browngreen    = QColor(106, 103, 19)
-  lightbrown    = QColor(192, 190, 139, 70)
-  birch         = QColor(59, 53, 40)
+  white             = QColor(255, 255, 255)
+  lightpink         = QColor(255, 248, 248)
+  middlepink        = QColor(221, 194, 194)
+  lightburgundy     = QColor(156, 78, 78)
+  darkred           = QColor(72, 8, 8)
+  lightgreen_popup  = QColor(237, 242, 239, 255)
+  lightgreen        = QColor(157, 182, 170, 60)
+  lightgreen_table  = QColor(157, 182, 170, 40)
+  middlegreen       = QColor(103, 126, 112, 80) 
+  darkgreen         = QColor(40, 59, 48)
+  browngreen        = QColor(106, 103, 19)
+  lightbrown        = QColor(192, 190, 139, 70)
+  birch             = QColor(59, 53, 40)
+  gray              = QColor(133, 133, 133, 70)
+  lightgray         = QColor(30, 30, 30, 120)

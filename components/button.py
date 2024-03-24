8a385c0de1ead20bfb895ba4a9e5bddcc28cac4e
mildphan <85720997+Phanisara@users.noreply.github.com>
2024-03-24 17:09:34 +0700

    Add the docstring and comment

--- components/button.py
+++ components/button.py
@@ -4,6 +4,28 @@ from PyQt5.QtWidgets import *
 from components.color import *
 
 class Button():
+   """
+   A class representing a button widget with customizable style and functionality.
+
+   Attributes:
+      window (QWidget): The parent window or widget.
+      text (str): The text displayed on the button.
+      font_name (str): The font family name.
+      font_size (int): The font size.
+      pos_x (int): The x-coordinate position of the button.
+      pos_y (int): The y-coordinate position of the button.
+      size_x (int): The width of the button.
+      size_y (int): The height of the button.
+      style (str): The style sheet for the button.
+      Goto (function, optional): The function to call when the button is clicked.
+      cursor (QCursor, optional): The cursor shape for the button.
+
+   Methods:
+      __init__: Initializes the Button object.
+      Icon: Sets the icon for the button.
+      disable: Disables the button.
+      enable: Enables the button.
+   """
    def __init__(self, window, text, font_name, font_size, pos_x, pos_y, size_x, size_y, style, Goto=None, cursor=None):
       self.style = style
       self.object = QPushButton(window)
@@ -19,16 +41,71 @@ class Button():
       self.ready = False
 
    def Icon(self, pic):
+      """
+      Function to set the icon for the button.
+
+      Args:
+         pic (QPixmap or QIcon): The icon to set for the button.
+      """
       self.object.setIcon(pic)
 
-   def disable(self, style_disable):
-      self.object.setStyleSheet(style_disable)
+   def disable(self, style=None):
+      """
+      Function to disable the button.
+
+      Args:
+         style (str, optional): The style sheet for the disabled button.
+      """
+      if style:
+         self.object.setStyleSheet(style)
+      self.object.setEnabled(False)
+
+   def enable(self):
+      """
+      Function to enable the button.
+      """
+      self.object.setStyleSheet(self.style)
+      self.object.setEnabled(True)
 
 class ProfileButton(QGraphicsObject):
+   """
+   A class representing a profile button widget with customizable style and functionality.
+
+   Attributes:
+      clicked_text (pyqtSignal): A signal emitted when the button is clicked, carrying text and object data.
+      window (QWidget): The parent window or widget.
+      text (str): The text displayed on the button.
+      font_name (str): The font family name.
+      font_size (int): The font size.
+      pos_x (int): The x-coordinate position of the button.
+      pos_y (int): The y-coordinate position of the button.
+      size_x (int): The width of the button.
+      size_y (int): The height of the button.
+      style (str): The style sheet for the button.
+      blur_radius (int): The blur radius for the button's shadow effect.
+      blur_color (QColor): The color of the button's shadow effect.
+      width_text (int): The width of the text label.
+      width_icon (int): The width of the icon label.
+      Goto (function, optional): The function to call when the button is clicked.
+      cursor (QCursor, optional): The cursor shape for the button.
+
+   Methods:
+      __init__: Initializes the ProfileButton object.
+      set_icon: Sets the icon for the button.
+      set_style: Sets the style for the button.
+      enterEvent: Event handler for mouse enter event.
+      leaveEvent: Event handler for mouse leave event.
+      on_clicked: Event handler for button clicked event.
+      text: Gets the text displayed on the button.
+      disable: Disables the button.
+      enable: Enables the button.
+   """
    clicked_text = pyqtSignal(str, object)
    def __init__(self, window, text, font_name, font_size, pos_x, pos_y, size_x, size_y, style, blur_radius, blur_color, width_text, width_icon, Goto=None, cursor=None):
       super().__init__() 
       self.style = style
+      self.blur_radius = blur_radius
+      self.blur_color = blur_color
       self.text_font_name = font_name
       self.text_font_size = font_size
       self.object = QPushButton(window)
@@ -70,30 +147,53 @@ class ProfileButton(QGraphicsObject):
 
       # Connect clicked signal to slot
       self.object.clicked.connect(self.on_clicked)
-
       self.click_flag = False
 
    def set_icon(self, icon_path, icon_width, icon_height):
+      """
+      Function to set the icon for the button.
+
+      Args:
+         icon_path (str): The file path of the icon.
+         icon_width (int): The width of the icon.
+         icon_height (int): The height of the icon.
+      """
       icon = QIcon(icon_path)
       self.icon_label.setAlignment(Qt.AlignCenter)
       self.icon_label.setPixmap(icon.pixmap(QSize(icon_width, icon_height)))
       self.icon_label.setStyleSheet("background-color: transparent;")
    
    def set_style(self, style, old_object=False):
+      """
+      Function to set the style for the button.
+
+      Args:
+         style (str): The style sheet for the button.
+         old_object (bool, optional): Indicates if the previous style should be reset.
+      """
       self.object.setStyleSheet(style)
       if old_object:
          self.click_flag = False
          self.text_label.setStyleSheet(f"color: {Color.darkgreen}; background-color: transparent; font-weight: bold;")
       
    def enterEvent(self, event):
+      """
+      Function to event handler for mouse enter event.
+      """
       if not self.click_flag:
             self.text_label.setStyleSheet(f"color: {Color.white}; background-color: transparent; font-weight: bold;")
 
    def leaveEvent(self, event):
+      """
+      Function to event handler for mouse leave event.
+      """
       if not self.click_flag:
-            self.text_label.setStyleSheet(f"color: {Color.darkgreen}; background-color: transparent; font-weight: bold;")
+         self.text_label.setStyleSheet(f"color: {Color.darkgreen}; background-color: transparent; font-weight: bold;")
 
    def on_clicked(self):
+      """
+      Function to event handler for button clicked event.
+      """
       text = self.text_label.text()
       self.click_flag = True
       self.clicked_text.emit(text, self) 
